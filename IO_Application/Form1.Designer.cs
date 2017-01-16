266e3f46783819fc5479a47b2180f9051eb019c9
Jordon Tijerina <jordontijerina@gmail.com>
2017-01-16 11:38:26 -0500

    First Commit

---
+++ IO_Application/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace IO_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(556, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Search For Path";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(537, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(649, 583);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 635);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ IO_Application/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace IO_Application {
+
+    public partial class Form1 : Form {
+
+        private int ACCURACY_RANGE = 1000;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+
+            // Gets the path from the textbox
+            string pic_Path = textBox1.Text;
+
+            try { 
+
+                OpenFileDialog open = new OpenFileDialog();
+
+                if (open.ShowDialog() == DialogResult.OK) {
+                    Bitmap bm = new Bitmap(open.FileName);
+
+                    textBox1.Text = open.FileName;
+
+                    int bmWidth = bm.Width;
+                    int bmHeight = bm.Height;
+
+                    int boxWidth = pictureBox1.Width;
+                    int boxHeight = pictureBox1.Height;
+
+                    double resize_X = 1, resize_Y = 1;
+
+                    Bitmap newBitmap = new Bitmap(boxWidth, boxHeight);
+
+                    if(bmWidth > boxWidth || bmHeight > boxHeight) {
+
+                        // Work on resizing the x axis
+                        if (bmWidth > boxWidth) {
+                            resize_X = ((bmWidth * ACCURACY_RANGE) / boxWidth);
+                            resize_X /= ACCURACY_RANGE;
+                        }
+
+                        // Work on resizing the y axis
+                        if(bmHeight > boxHeight) {
+                            resize_Y = ((bmHeight * ACCURACY_RANGE) / boxHeight);
+                            resize_Y /= ACCURACY_RANGE;
+                        }
+
+
+                        for(int x = 0; x < boxWidth; x++) {
+                            for(int y = 0; y < boxHeight; y++) {
+
+                                newBitmap.SetPixel( x, y, bm.GetPixel((int)(x * resize_X), (int)(y * resize_Y)));
+
+                            }
+                        }
+
+                        pictureBox1.Image = newBitmap;
+
+                    }
+
+                    else {
+                        pictureBox1.Image = bm;
+                    }
+                    
+                }
+            }
+            catch (Exception) {
+                throw new ApplicationException("Failed loading image");
+            }
+
+        }
+    }
+}
